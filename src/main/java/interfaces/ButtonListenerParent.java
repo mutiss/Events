42323184dbf6697474293af694c6fe5e8b06bbfe
cdorado <cdorado@bluumi.net>
2018-02-12 11:50:49 +0100

    Subida

---
+++ src/main/java/interfaces/ButtonListenerParent.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+import com.eventwidgets.listeners.ButtonOnTouchListener;
+
+/**
+ * Created by Cdorado on 12/02/2018.
+ */
+
+public class ButtonListenerParent extends ButtonOnTouchListener {
+
+    public ButtonListenerParent(){
+        super();
+    }
+
+}
